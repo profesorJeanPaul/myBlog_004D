b36afef691630c1f9df4bc5f6bb9cfa2b39d8561
Profesor Jean Paul <jean.fuentes@profesor.duoc.cl>
2018-10-22 09:46:13 -0300

    Agrega los detalles de los post

--- myBlog/views.py
+++ myBlog/views.py
@@ -6,3 +6,7 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', { 'posts': posts })
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', { 'post': post })
