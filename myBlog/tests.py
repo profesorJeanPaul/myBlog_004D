cc2fa4d1c25cdbdc36f55e68b75561df315355ff
Profesor Jean Paul <jean.fuentes@profesor.duoc.cl>
2018-10-24 09:31:58 -0300

    Agrega los Unit test a la app

--- myBlog/tests.py
+++ myBlog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+from .models import Post
+
 # Create your tests here.
+class BlogTest(TestCase):
+    def testSum(self):
+        # Arrange
+        expected = 2
+        result = -1
+        # Act
+        result = 1 + 1
+        # Assert
+        self.assertEqual(expected, result)
